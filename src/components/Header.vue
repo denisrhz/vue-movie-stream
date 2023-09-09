57e496a20b718c965cc9c8227b758a18c4c4b3cb
denisrhz <denise.great51@gmail.com>
2023-09-09 22:05:34 +0300

    Add SearchInput to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,8 +10,15 @@
                 <img src="https://s2.bunnycdn.ru/assets/sites/zoro/logo.png" class="w-24" alt="logo">
             </div>
             <div class="grow"></div>
-            <div class="flex-none my-auto mx-2">
-                <button class="p-1"><font-awesome-icon :icon="['fas', 'search']"/></button>
+            <div class="flex-none relative my-auto mx-2">
+                <input
+                type="search"
+                class="block w-64 p-1 text-sm text-gray-600 flex-auto rounded-sm border border-solid border-gray-700 bg-clip-padding px-3 font-normal leading-[1.6] outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-gray-600 focus:text-gray-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none"
+                :value="searchQuery"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon1" />
+                <button class="absolute right-2 top-1"><font-awesome-icon class="text-midnight" :icon="['fas', 'search']"/></button>
             </div>
             <div class="flex-none my-auto mx-2 justify-self-end">
                 <button class="w-full px-5 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-purple-600 rounded-md sm:mx-2 sm:order-2 sm:w-auto hover:bg-purple-500 focus:outline-none focus:ring focus:ring-purple-300 focus:ring-opacity-20">Login</button>
