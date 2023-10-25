9a19c95b181a70fe177af2a879d96c3797632241
denisrhz <denise.great51@gmail.com>
2023-10-25 15:11:09 +0300

    Updated MovieView styles

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,27 +1,21 @@
 <template>
     <div>
-        <ol class="flex items-center gap-4 rounded-md px-4 py-2 shadow-md">
-            <li>
-                <div class="flex items-center text-lg font-medium opacity-80 transition-all duration-300 hover:text-pink-500">
-                    <svg class="mr-2.5 h-3 w-3 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-                    </svg>
+        <ol class="flex items-center gap-4 rounded-md px-4 py-3 shadow-md">
+            <li class="inline-flex">
+                <div class="flex items-center gap-2 font-medium opacity-80 transition-colors duration-300 hover:text-primary">
+                    <font-awesome-icon :icon="['fas', 'home']" />
                     <a href="#">Главная</a>
                 </div>
             </li>
             <li class="inline-flex">
-                <div class="flex items-center gap-2 text-lg font-medium opacity-80 transition-all duration-300 hover:text-pink-500">
-                    <svg class="h-3 w-3 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                    </svg>
+                <div class="flex items-center gap-2 font-medium opacity-80 transition-colors duration-300 hover:text-primary">
+                    <font-awesome-icon :icon="['fas', 'chevron-right']" />
                     <a href="">Фильмы</a>
                 </div>
             </li>
             <li class="inline-flex">
-                <div class="flex items-center gap-2 text-lg font-medium opacity-80 transition-all duration-300 hover:text-pink-500">
-                    <svg class="h-3 w-3 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                    </svg>
+                <div class="flex items-center gap-2 font-medium opacity-80 transition-colors duration-300 hover:text-primary">
+                    <font-awesome-icon :icon="['fas', 'chevron-right']" />
                     <a href="">Название</a>
                 </div>
             </li>
--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,34 +1,98 @@
 <template>
-    <div class="">
-        <Breadcrumbs/>
-        <iframe class="w-full h-[60vw] max-h-[75vh] mb-4" :src="playerLink" allowfullscreen></iframe>
-        <div class="container mx-auto p-4">
-            <h1 class="text-3xl">{{ movieDetails.title }}</h1>
-            <span class="text-primary">{{ movieDetails.vote_average }}</span> * <span>{{ movieDetails.vote_count }}</span>
-            <p class="">{{ movieDetails.overview }}</p>
+    <Breadcrumbs/>
+    <iframe 
+    :src="playerLink" allowfullscreen
+    class="w-full h-[60vw] max-h-[75vh] mb-4" 
+    >
+    </iframe>
+    <div class="container mx-auto p-3">
+        <div class="flex">
+            <div class="flex-none w-64 shadow-lg">
+                <img v-if="movieDetails.poster_path" :src="posterPath" alt="" class="">
+            </div>
+            <div class="ml-5 p-2">
+                <h1 class="text-4xl">{{ movieDetails.title }}</h1>
+                <div class="my-3">
+                    <span 
+                    class="w-full mx-1 px-2 py-1.5 text-sm tracking-wide text-white bg-purple-600 rounded-md">
+                    <font-awesome-icon :icon="['fas', 'star']" />
+                    {{ movieDetails.vote_average?.toFixed(1) }}
+                    </span>
+                    <span 
+                    class="w-full mx-1 px-2 py-1.5 text-sm tracking-wide text-white rounded-md">
+                    {{ movieDetails.release_date?.slice(0, 4) }}
+                    </span>
+                    <span 
+                    class="w-full mx-1 px-2 py-1.5 text-sm tracking-wide text-white rounded-md">
+                    {{ formatRuntime }}
+                    </span>
+                </div>
+                <p v-if="movieDetails.tagline" class="text-primary">{{ movieDetails.tagline }}</p>
+                <p class="my-2 text-gray-300">{{ movieDetails.overview }}</p>
+                <div class="flex gap-4">
+                    <div class="flex flex-col">
+                        <div class="flex flex-col mb-3">
+                            <span class="text-gray-500 mb-1">Оригинальное название:</span>
+                            <span class="">{{ movieDetails.original_title || "-" }}</span>
+                        </div>
+                        <div class="flex flex-col mb-3">
+                            <span class="text-gray-500 mb-1">Жанры:</span>
+                            <span class="">{{ null || "-" }}</span>
+                        </div>
+                        <div class="flex flex-col mb-3">
+                            <span class="text-gray-500 mb-1">Страны:</span>
+                            <span class="">{{ null || "-" }}</span>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <div class="flex flex-col mb-3">
+                            <span class="text-gray-500 mb-1">Продолжительность:</span>
+                            <span class="">{{ formatRuntime || "-" }}</span>
+                        </div>
+                        <div class="flex flex-col mb-3">
+                            <span class="text-gray-500 mb-1">Дата выхода:</span>
+                            <span class="">{{ movieDetails.release_date || "-" }}</span>
+                        </div>
+                        <div class="flex flex-col mb-3">
+                            <span class="text-gray-500 mb-1">Компании:</span>
+                            <span class="">{{ null || "-" }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
+
 <script>
 import Breadcrumbs from '../components/Breadcrumbs.vue';
 
 export default {
     components: { Breadcrumbs },
-
+    
     data() {
         return {
-            movieDetails: {}
+            movieDetails: {},
+            movieCollection: {}
         };
     },
-
+    
     computed: {
         playerLink() {
-            return "https://vidsrc.to/embed/movie/"; // + this.$route.params.id
+            return "https://vidsrc.to/embed/movie/" // + this.$route.params.id
+        },
+        posterPath() {
+            return 'https://image.tmdb.org/t/p/w500/' + this.movieDetails.poster_path
+        },
+        formatRuntime() {
+            let mins = this.movieDetails.runtime;
+            return `${Math.trunc(mins/60)} ч ${mins % 60} мин`;
         }
     },
-
+    
     mounted() {
         this.getMovieDetails();
+        // this.getMovieCollection();
     },
     
     methods: {
@@ -41,9 +105,24 @@ export default {
                 }
             };
             fetch(`https://api.themoviedb.org/3/movie/${this.$route.params.id}?language=ru-RU`, options)
-                .then(response => response.json())
-                .then(response => this.movieDetails = response)
-                .catch(err => console.error(err));
+            .then(response => response.json())
+            .then(response => this.movieDetails = response)
+            .catch(err => console.error(err));
+        },
+        getMovieCollection() {
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZGNiOTBlYmY3ZDE5NmRkNWQyMjRmMzg4MWM4M2JjZCIsInN1YiI6IjY0Y2Q5NzA5NTQ5ZGRhMDExYzI3M2ZlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8hREDgRimJl9MjrwuqXNP81ptiRYyIxma3ki19xQL8Y'
+                }
+            };
+            
+            fetch(`https://api.themoviedb.org/3/collection/${this.movieDetails.belongs_to_collection.id}?language=ru-RU`, options)
+            .then(response => response.json())
+            .then(response => this.movieCollection = response)
+            .catch(err => console.error(err));
+            
         }
     },
 }
